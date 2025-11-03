a5ae2624218f22c3ed83df88007afd943070efc3
Ethan <ethancusi2005@gmail.com>
2025-11-02 23:14:28 -0500

    feat:api-galleta

---
+++ Integration/galletafortuna/dto/Galleta.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PROYECTO_RESERVASRESTAURANTES.Integration.galletafortuna.dto
+{
+
+ public class Galleta
+    {
+        public string Mensaje { get; set; }
+    }
+
+}
